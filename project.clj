bf68bdf2a6b07f5e8f66290ae1f23ee3d689e296
James Petry <gavilancomun@gmail.com>
2013-11-20 11:19:01 +0000

    initial working

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject mozart-clj/mozart-game "0.0.1-SNAPSHOT"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [overtone/at-at "1.2.0"]
+                 [overtone/midi-clj "0.5.0"]
+                 [overtone/overtone "0.8.0"]]
+  :description "Mozart musical dice game"
+  :plugins [[lein-kibit "0.0.8"]]
+  :license {:name "Eclipse Public License 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/gavilancomun/mozart-clj")
